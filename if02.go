544f81166c02ff7a662a63f09cbb93eee40723e0
Cube-Y <69599304+Cube-Y@users.noreply.github.com>
2020-09-03 20:39:34 +0900

    Add files via upload

---
+++ if02.go
@@ -0,0 +1,25 @@
+// coding Go-Lang : utf-8
+package main
+import "fmt"
+
+func main() {
+
+	OneHundred := 100
+	Ten := 10
+	Way := "Sorry..."
+
+	if OneHundred < Ten {
+		fmt.Println(Ten)
+	}else if OneHundred > Ten {
+		fmt.Println(OneHundred)
+	}else {
+		fmt.Println(Way)
+	}
+}
+
+
+var string {
+
+
+
+
